ba8b50d576895c6a445da33eae7f72ebf71a21cf
Freddie Cowgill <rfcowgill@att.net>
2019-08-27 12:36:14 -0400

    Initial Commit

---
+++ Simple Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Weather
+//
+//  Created by Frederick Cowgill on 8/27/19.
+//  Copyright © 2019 Frederick Cowgill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
